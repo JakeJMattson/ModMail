380b5e1350bec48fe3ce443f1eefa00385eb34c7
aberrantfox <aberrantfoxtph@gmail.com>
2018-05-10 22:30:13 +0100

    Addition of missing files

---
+++ src/main/kotlin/me/aberrantfox/warmbot/commands/ReportCommands.kt
@@ -0,0 +1,23 @@
+package me.aberrantfox.warmbot.commands
+
+import me.aberrantfox.kjdautils.api.dsl.CommandSet
+import me.aberrantfox.kjdautils.api.dsl.commands
+import me.aberrantfox.warmbot.ObjectRegister
+import me.aberrantfox.warmbot.services.ReportService
+import net.dv8tion.jda.core.entities.TextChannel
+
+@CommandSet
+fun reportCommands() = commands {
+    command("close") {
+        execute {
+            val reportService = ObjectRegister["reportService"] as ReportService
+
+            if( !(reportService.isReportChannel(it.channel.id)) ) {
+                it.respond("Nice try, but you can't close a channel that isn't a report. That would be silly. Don't do silly things.")
+                return@execute
+            }
+
+            (it.channel as TextChannel).delete().queue()
+        }
+    }
+}
