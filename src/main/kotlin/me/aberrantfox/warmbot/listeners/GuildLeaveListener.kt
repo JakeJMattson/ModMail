2758daccdb4fc014c1089d65f4a2e913967ff562
Jake <JakeJMattson@gmail.com>
2018-09-21 18:52:48 -0500

    Remove ban event
    Removed ban event method in favor of checking to see if a leave was a ban.

--- src/main/kotlin/me/aberrantfox/warmbot/listeners/GuildLeaveListener.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/listeners/GuildLeaveListener.kt
@@ -9,40 +9,16 @@ import java.awt.Color
 
 class GuildLeaveListener(private val reportService: ReportService, val configuration: Configuration) {
 
-	private var wasBanned = false
-
 	@Subscribe
 	fun onGuildLeave(event: GuildMemberLeaveEvent) {
 
-		//Ignore leave event if ban was reported
-		if (wasBanned) {
-			wasBanned = false
-			return
-		}
-
 		val user = event.user.id
-		val message = "has left"
 
-		notifyReportChannel(user, message)
-	}
-
-	@Subscribe
-	fun onGuildBan(event: GuildBanEvent) {
-
-		wasBanned = true
-
-		val user = event.user.id
-		val message = "was banned from"
-
-		notifyReportChannel(user, message)
-	}
-
-	private fun notifyReportChannel(user: String, message: String)
-	{
 		if (reportService.hasReportChannel(user)) {
 
 			val reportId = reportService.getReportByUserId(user).channelId
 			val reportChannel = reportService.jda.getTextChannelById(reportId)
+			val message = if(event.guild.banList.complete().any { b -> b.user.id == user }) "was banned from" else "has left"
 			val embed = EmbedBuilder().addField("User no longer in server!", "This user $message the server.", false)
 			embed.setColor(Color.red)
 
