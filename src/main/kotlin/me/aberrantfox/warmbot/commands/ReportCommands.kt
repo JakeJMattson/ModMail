1cdf6441e17010a340146f68e6eab40da4793f8a
Jacob Mattson <JakeJMattson@gmail.com>
2018-11-11 16:39:20 -0600

    Command refactor
    Make initial message optional and modify the embed message accordingly.
    Fix embed in report to accurately reflect the target user instead of the staff member.
    Combine embeds.
    Detect closed DM's in a better way.
    Minimize variable scope.

--- src/main/kotlin/me/aberrantfox/warmbot/commands/ReportCommands.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/commands/ReportCommands.kt
@@ -12,85 +12,77 @@ import java.util.concurrent.ConcurrentHashMap
 
 @CommandSet
 fun reportCommands(reportService: ReportService, configuration: Configuration) = commands {
-    command("open") {
-        description = "Open a report with the target user and send the provided initial message."
-        expect(arg(UserArg), arg(SentenceArg("Initial Message"), optional = true, default = ""))
-        execute { event ->
-            val targetUser = event.args.component1() as User
-            val message = event.args.component2() as String
-            val guild = event.message.guild
+	command("open") {
+		description = "Open a report with the target user and send the provided initial message."
+		expect(arg(UserArg), arg(SentenceArg("Initial Message"), optional = true, default = ""))
+		execute { event ->
+			val targetUser = event.args.component1() as User
+			var message = event.args.component2() as String
+			val guild = event.message.guild
 
-            if (targetUser.isBot) {
-                event.respond("The target user is a bot.")
-                return@execute
-            }
+			if (targetUser.isBot) {
+				event.respond("The target user is a bot.")
+				return@execute
+			}
 
-            if (!guild.isMember(targetUser)) {
-                event.respond("The target user is not in this guild.")
-                return@execute
-            }
+			if (!guild.isMember(targetUser)) {
+				event.respond("The target user is not in this guild.")
+				return@execute
+			}
 
-            if (reportService.hasReportChannel(targetUser.id)) {
-                event.respond("The target user already has an open report.")
-                return@execute
-            }
+			if (reportService.hasReportChannel(targetUser.id)) {
+				event.respond("The target user already has an open report.")
+				return@execute
+			}
 
-            targetUser.openPrivateChannel().queue outerQueue@ {
-                val userEmbed = embed {
-                    setColor(Color.green)
-                    setThumbnail(guild.iconUrl)
-                    addField("You've received a message from the staff of ${guild.name}!",
-                        "This is a two-way communication medium between you and the entire staff team. " +
-                                "Reply directly into this channel and your message will be forwarded to them.",
-                        false)
-                }
+			val userEmbed = embed {
+				setColor(Color.green)
+				setThumbnail(guild.iconUrl)
+				addField("You've received a message from the staff of ${guild.name}!",
+					"This is a two-way communication medium between you and the entire staff team. " +
+							"Reply directly into this channel and your message will be forwarded to them.",
+					false)
+			}
 
-                val embedMessage = if (message.isEmpty()) "<No initial message provided>" else message
+			targetUser.openPrivateChannel().queue {
+				it.sendMessage(userEmbed).queue({
 
-                val staffEmbed = embed {
-                    setColor(Color.yellow)
-                    addField("This report was opened by a staff member!",
-                        "Report opened by **${event.author.name}** (${event.author.id})",
-                        false)
-                    addField("Initial Message", embedMessage, false)
-                }
+					val guildConfiguration = configuration.guildConfigurations.first { g -> g.guildId == guild.id }
+					val reportCategory = reportService.jda.getCategoryById(guildConfiguration.reportCategory)
 
-                val guildConfiguration = configuration.guildConfigurations.first { g -> g.guildId == guild.id }
-                val reportCategory = reportService.jda.getCategoryById(guildConfiguration.reportCategory)
+					reportCategory.createTextChannel(targetUser.name).queue { channel ->
+						channel as TextChannel
 
-                targetUser.sendPrivateMessage(userEmbed, DefaultLogger())
+						if (message.isEmpty())
+							message = "<No initial message provided>"
+						else
+							targetUser.sendPrivateMessage(message, DefaultLogger())
 
-                if (message.isNotEmpty())
-                    targetUser.sendPrivateMessage(message, DefaultLogger())
+						channel.sendMessage(
+							embed {
+								addField("New Report Opened!",
+									"${targetUser.descriptor()} :: ${targetUser.asMention}",
+									false)
+								addField("This report was opened by a staff member!",
+									"${event.author.descriptor()} :: ${event.author.asMention}",
+									false)
+								addField("Initial Message", message, false)
+								setColor(Color.green)
+							}).queue()
 
-                it.getMessageById(it.latestMessageId).queue { latestMessage ->
-                    if (latestMessage.contentRaw.trim() != message.trim() && latestMessage.embeds.isNotEmpty()) {
-                        event.respond("Unable to contact the target user. DM's may be disabled.")
-                        return@queue
-                    }
+						val newReport = Report(targetUser.id, channel.id, guild.id, ConcurrentHashMap(), null)
+						reportService.reports.add(newReport)
+						reportService.writeReportToFile(newReport)
 
-                    reportCategory.createTextChannel(targetUser.name).queue { channel ->
-                        channel as TextChannel
-
-                        val openingMessage = embed {
-                            addField("New Report Opened!", "${event.author.descriptor()} :: ${event.author.asMention}",
-                                false)
-                            setColor(Color.green)
-                        }
-
-                        channel.sendMessage(openingMessage).queue()
-                        channel.sendMessage(staffEmbed).queue()
-
-                        val newReport = Report(targetUser.id, channel.id, guild.id, ConcurrentHashMap(), null)
-                        reportService.reports.add(newReport)
-                        reportService.writeReportToFile(newReport)
-
-                        event.respond("Channel opened at: ${channel.asMention}")
-                    }
-                }
-            }
-        }
-    }
+						event.respond("Channel opened at: ${channel.asMention}")
+					}
+				},
+				{
+					event.respond("Unable to contact the target user. Direct messages are disabled.")
+				})
+			}
+		}
+	}
 
     command("close") {
         description = "Close the report channel that this command is invoked in. Alternatively, delete the channel."
