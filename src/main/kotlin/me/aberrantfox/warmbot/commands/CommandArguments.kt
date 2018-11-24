6f5e926688149914c6a225d6017d0e2afa60ca43
Jake Mattson <JakeJMattson@gmail.com>
2018-11-23 23:59:55 -0600

    Update CommandArguments.kt
    Implement "when with subject"

--- src/main/kotlin/me/aberrantfox/warmbot/commands/CommandArguments.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/commands/CommandArguments.kt
@@ -2,10 +2,7 @@ package me.aberrantfox.warmbot.commands
 
 import me.aberrantfox.kjdautils.api.dsl.CommandEvent
 import me.aberrantfox.kjdautils.extensions.stdlib.trimToID
-import me.aberrantfox.kjdautils.internal.command.ArgumentResult
-import me.aberrantfox.kjdautils.internal.command.ArgumentType
-import me.aberrantfox.kjdautils.internal.command.ConsumptionType
-import me.aberrantfox.kjdautils.internal.command.tryRetrieveSnowflake
+import me.aberrantfox.kjdautils.internal.command.*
 
 object ChannelCategoryArg : ArgumentType {
 
@@ -15,8 +12,8 @@ object ChannelCategoryArg : ArgumentType {
 
     override fun convert(arg: String, args: List<String>, event: CommandEvent): ArgumentResult {
         val retrieved = tryRetrieveSnowflake(event.jda) { it.getCategoryById(arg.trimToID()) }
-        return when {
-            retrieved == null -> ArgumentResult.Error("Couldn't retrieve channel category: $arg")
+        return when (retrieved) {
+            null -> ArgumentResult.Error("Couldn't retrieve channel category: $arg")
             else -> ArgumentResult.Single(retrieved)
         }
     }
