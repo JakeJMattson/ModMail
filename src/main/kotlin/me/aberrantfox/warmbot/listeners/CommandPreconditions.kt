570876a2a7260f8f79d48c81c3505a4bda18e103
Jake Mattson <JakeJMattson@gmail.com>
2018-12-05 17:02:38 -0600

    Add Staff Channel Precondition

--- src/main/kotlin/me/aberrantfox/warmbot/listeners/CommandPreconditions.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/listeners/CommandPreconditions.kt
@@ -5,6 +5,24 @@ import me.aberrantfox.kjdautils.internal.command.*
 import me.aberrantfox.warmbot.services.Configuration
 import net.dv8tion.jda.core.entities.TextChannel
 
+fun produceIsStaffChannelPrecondition(configuration: Configuration) = { event: CommandEvent ->
+
+    if (event.channel is TextChannel) {
+        val textChannel = event.channel as TextChannel
+
+        if (configuration.hasGuildConfig(textChannel.guild.id)) {
+            val relevantGuildConfiguration = configuration.getGuildConfig(textChannel.guild.id)!!
+            val relevantGuild = event.jda.getGuildById(textChannel.guild.id)
+            val staffRole = relevantGuild.getRolesByName(relevantGuildConfiguration.staffRoleName, true).first()
+            val perms = textChannel.getPermissionOverride(staffRole)
+
+            if (perms == null) Fail() else Pass
+        }
+        else Pass
+    }
+    else Fail()
+}
+
 fun produceIsStaffMemberPrecondition(configuration: Configuration) = { event: CommandEvent ->
 
     if (event.channel is TextChannel) {
