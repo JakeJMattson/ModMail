f445e92811a780aa9d5e08fbd84b89b1067e4450
AberrantFox <aberrantfoxtph@gmail.com>
2018-12-07 05:34:20 +0000

    Refactor extensions

--- src/main/kotlin/me/aberrantfox/warmbot/extensions/ChannelExtension.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/extensions/ChannelExtension.kt
@@ -3,37 +3,23 @@ package me.aberrantfox.warmbot.extensions
 import me.aberrantfox.kjdautils.extensions.jda.fullName
 import net.dv8tion.jda.core.entities.MessageChannel
 
-fun MessageChannel.archiveString(prefix: String): String {
+private const val embedNotation = "<---------- Embed ---------->"
 
-	val newline = System.lineSeparator()
-	val embedNotation = "<---------- Embed ---------->"
+fun MessageChannel.archiveString(prefix: String) =
+    iterableHistory.reversed()
+        .filter { !it.contentRaw.toLowerCase().matches(Regex("($prefix){1,2}archive")) }
+        .joinToString(System.lineSeparator()) {
+            val stringBuilder = StringBuilder("${it.author.fullName()}: ")
 
-	return iterableHistory.reversed().filter { !it.contentRaw.toLowerCase().matches(Regex("($prefix){1,2}archive")) }
-		.joinToString(System.lineSeparator()) {
+            if (it.embeds.isNotEmpty()) {
+                it.embeds.forEach {
+                    stringBuilder.appendln(embedNotation)
+                    it.fields.forEach { field -> stringBuilder.appendln(field.name).appendln(field.value) }
+                    stringBuilder.appendln(embedNotation)
+                }
+            } else {
+                stringBuilder.append(it.fullContent())
+            }
 
-			val stringBuilder = StringBuilder("${it.author.fullName()}: ")
-
-			if (it.embeds.isNotEmpty()) {
-				it.embeds.forEach {
-
-					stringBuilder.append(newline + embedNotation + newline)
-					val fieldCount = it.fields.size
-
-					it.fields.forEachIndexed { index, field ->
-						stringBuilder
-							.append(field.name).append(newline)
-							.append(field.value).append(newline)
-
-						if (index != fieldCount - 1)
-							stringBuilder.append(newline)
-					}
-
-					stringBuilder.append(embedNotation).append(newline)
-				}
-			}
-			else
-				stringBuilder.append(it.fullContent())
-
-			stringBuilder.toString()
-		}
-}
+            stringBuilder.toString()
+        }
