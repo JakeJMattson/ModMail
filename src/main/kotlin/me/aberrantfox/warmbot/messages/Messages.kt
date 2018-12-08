8664557cb24c89ecff5ce77c03ca93d54a4dd068
AberrantFox <aberrantfoxtph@gmail.com>
2018-12-08 19:23:28 +0000

    Additional localisation

--- src/main/kotlin/me/aberrantfox/warmbot/messages/Messages.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/messages/Messages.kt
@@ -2,13 +2,14 @@ package me.aberrantfox.warmbot.messages
 
 
 class Messages (
+    val COMMON_NO_MESSAGE: String,
     val SET_REPORT_CATEGORY_DESCRIPTION: String,
-    val REPORT_ARCHIVE_SUCCESSFUL: String,
-    val SET_ARCHIVE_CHANNEL_DESCRIPTION: String,
-    val ARCHIVE_CHANNEL_SET_SUCCESSFUL: String,
-    val SET_STAFF_ROLE_DESCRIPTION: String,
-    val COULD_NOT_FIND_ROLE: String,
     val SET_STAFF_ROLE_SUCCESSFUL: String,
+    val SET_ARCHIVE_CHANNEL_DESCRIPTION: String,
+    val SET_STAFF_ROLE_DESCRIPTION: String,
     val SETUP_DESCRIPTION: String,
+    val REPORT_ARCHIVE_SUCCESSFUL: String,
+    val ARCHIVE_CHANNEL_SET_SUCCESSFUL: String,
+    val COULD_NOT_FIND_ROLE: String,
     val NO_CONFIG: String
 )
