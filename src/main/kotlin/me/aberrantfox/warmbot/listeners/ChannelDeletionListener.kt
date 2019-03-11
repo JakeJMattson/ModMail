e423fdf5d9f01e6c41bcd370dfce22ae04af2354
Jake Mattson <JakeJMattson@gmail.com>
2019-03-10 19:49:58 -0500

    Create AuditLogPollingService
    Created a service for polling the audit log and listening for channel deletion entries. If one is found, log it.

--- src/main/kotlin/me/aberrantfox/warmbot/listeners/ChannelDeletionListener.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/listeners/ChannelDeletionListener.kt
@@ -4,8 +4,14 @@ import com.google.common.eventbus.Subscribe
 import me.aberrantfox.warmbot.services.*
 import net.dv8tion.jda.core.events.channel.text.TextChannelDeleteEvent
 
-class ChannelDeletionListener(private val service: ReportService) {
+class ChannelDeletionListener(private val reportService: ReportService, private val auditLogPollingService: AuditLogPollingService) {
     @Subscribe
-    fun onTextChannelDelete(event: TextChannelDeleteEvent) =
-        if (event.channel.isReportChannel()) event.channel.channelToReport().let { service.closeReport(it) } else Unit
+    fun onTextChannelDelete(event: TextChannelDeleteEvent) {
+        val channel = event.channel
+
+        if (channel.isReportChannel()) {
+            auditLogPollingService.registerChannel(channel)
+            reportService.closeReport(channel.channelToReport())
+        }
+    }
 }
