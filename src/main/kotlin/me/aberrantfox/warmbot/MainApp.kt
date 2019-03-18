de1c4208ff0396c0bc7e8b42dd232608bf7e8421
Jake Mattson <JakeJMattson@gmail.com>
2019-03-17 23:19:27 -0500

    Remove hard-coded prefix; config only

--- src/main/kotlin/me/aberrantfox/warmbot/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/MainApp.kt
@@ -1,7 +1,12 @@
 package me.aberrantfox.warmbot
 
+import me.aberrantfox.kjdautils.api.annotation.Service
+import me.aberrantfox.kjdautils.api.dsl.KJDAConfiguration
 import me.aberrantfox.kjdautils.api.startBot
+import me.aberrantfox.warmbot.services.Configuration
 import net.dv8tion.jda.core.entities.Game
+
+private lateinit var kjdaConfig: KJDAConfiguration
 
 fun main(args: Array<String>) {
     val token = args.first()
@@ -13,10 +18,13 @@ fun main(args: Array<String>) {
 
     startBot(token) {
         configure {
-            prefix = "!"
+            kjdaConfig = this
             globalPath = "me.aberrantfox.warmbot"
         }
 
         jda.presence.setPresence(Game.of(Game.GameType.DEFAULT, "DM to contact Staff"), true)
     }
 }
+
+@Service
+class prefixLoader(configuration: Configuration) { init { kjdaConfig.prefix = configuration.prefix } }
