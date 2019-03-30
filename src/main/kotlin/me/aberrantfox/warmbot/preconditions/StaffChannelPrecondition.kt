0be84f6e4c75a69d22542882e73e58d7a5056cfd
Jake Mattson <JakeJMattson@gmail.com>
2019-03-30 18:06:02 -0500

    Fix block on report channels

--- src/main/kotlin/me/aberrantfox/warmbot/preconditions/StaffChannelPrecondition.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/preconditions/StaffChannelPrecondition.kt
@@ -13,7 +13,10 @@ fun produceIsStaffChannelPrecondition(configuration: Configuration) = exit@{ eve
 
     val guildConfig = configuration.getGuildConfig(textChannel.guild.id) ?: return@exit Pass
 
-    if (textChannel.id !in guildConfig.staffChannels) return@exit Fail()
+    val isWhitelisted = textChannel.id in guildConfig.staffChannels
+    val isReportCategory = textChannel.parent.id == guildConfig.reportCategory
+
+    if (!isWhitelisted && !isReportCategory) return@exit Fail()
 
     return@exit Pass
 }
