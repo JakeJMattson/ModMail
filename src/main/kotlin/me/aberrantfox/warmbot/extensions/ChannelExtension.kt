db392d0cd8f66080bfb732183ef5df50c79afa7e
Jake Mattson <JakeJMattson@gmail.com>
2019-04-21 00:45:40 -0500

    Fix archiving bug
    Fixed a bug in the archiving process where links would be perceived as embeds, and be incorrectly archived as such.

--- src/main/kotlin/me/aberrantfox/warmbot/extensions/ChannelExtension.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/extensions/ChannelExtension.kt
@@ -1,12 +1,12 @@
 package me.aberrantfox.warmbot.extensions
 
-import me.aberrantfox.kjdautils.extensions.jda.fullName
+import me.aberrantfox.kjdautils.extensions.jda.*
 import net.dv8tion.jda.core.entities.MessageChannel
 
 fun MessageChannel.archiveString() = iterableHistory.reversed().dropLast(1)
     .joinToString(System.lineSeparator()) {
         StringBuilder("${it.author.fullName()}: ").apply {
-            if (it.embeds.isNotEmpty() && it.author.isBot) {
+            if (it.embeds.isNotEmpty() && !it.containsURL()) {
                 it.embeds.forEach { embed ->
                     this.appendln()
                     this.append(embed.toTextString())
