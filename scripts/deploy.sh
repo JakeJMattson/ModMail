36caa25d3e06ef1c7e3f4dd0da0b6cf0314ee2bf
Jake Mattson <JakeJMattson@gmail.com>
2019-07-20 17:11:43 -0500

    Update Docker persistence

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t warmbot:latest -f docker/Dockerfile --no-cache .
-cmd="docker run -e BOT_TOKEN='$1' -v $2:/config warmbot:latest"
+cmd="docker run -e BOT_TOKEN='$1' -v $2:/data warmbot:latest"
 eval $cmd
