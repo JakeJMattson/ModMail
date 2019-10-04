32ba2d965811ffceb6c4af7b2bddafb7e0a2747b
Jake Mattson <JakeJMattson@gmail.com>
2019-10-04 08:12:41 -0500

    Update MentionListener.kt

--- src/main/kotlin/me/aberrantfox/warmbot/listeners/MentionListener.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/listeners/MentionListener.kt
@@ -1,6 +1,8 @@
 package me.aberrantfox.warmbot.listeners
 
 import com.google.common.eventbus.Subscribe
+import me.aberrantfox.kjdautils.extensions.stdlib.trimToID
+import me.aberrantfox.warmbot.extensions.selfUser
 import me.aberrantfox.warmbot.services.InfoService
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 
@@ -10,7 +12,7 @@ class MentionListener(private val infoService: InfoService) {
         with (event) {
             if (author.isBot) return
 
-            if (message.contentRaw == jda.selfUser.asMention)
+            if (message.contentRaw.trimToID() == selfUser().id)
                 channel.sendMessage(infoService.botInfo(channel)).queue()
         }
     }
