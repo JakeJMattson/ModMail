a05acc1cdc63b7ff64dc1a010ccce0cfa1136bc9
JakeJMattson <JakeJMattson@gmail.com>
2020-06-21 10:37:55 -0500

    Docs update

--- README.md
+++ README.md
@@ -1,55 +1,31 @@
-# WarmBot - A Report Management Discord Bot
+![KUtils Server](https://discordapp.com/api/guilds/453208597082406912/widget.png?style=banner2)
 
-WarmBot is a report management bot designed to provide a communication system between server staff and other members.
-In an ordinary server, users typically need to ping or message a staff member in order to get an issue resolved.
-This can create complications, such as the staff member not being online, not knowing the answer, being too busy to respond, or countless other issues.
-Other staff may be a perfect fit for the issue, but the member has no way of knowing who to message or ping.
-This can lead to passing around screenshots of conversations, information duplication, and in short, a big mess.
-WarmBot fixes this.
-
-![WarmBot](https://i.imgur.com/EJEC0Eu.png)
+# WarmBot
+WarmBot is a Discord bot designed to provide a communication system between server staff and other members.
 
 ## Reports
 Reports are private text channels that allow the entire staff team to communicate with a single member.
-You can have as many of these as you want (limited by configuration and Discord).
 
 ![Reports](https://i.imgur.com/7vgwc9E.png)
 
-## Member Experience
-### Opening a Report
-To open a report as a member, simply message the bot.
-A new report will be opened automatically.
-
-![Member](https://i.imgur.com/tf9k6PI.png)
-
-### Using a Report
-Once a report is opened, you can respond to the bot as if it were any other conversation.
-All messages sent to the bot will be forwarded to your report channel for staff to see.
-
-## Staff Experience
-### Opening a Report
-If a report needs to be opened from the staff side, you can use the `open` command.
-This will attempt to create a new report with the target user.
-This can be useful if you see a member misbehaving on the server but don't want to reprimand them publicly.
+### Creating Reports
+Reports will be opened automatically whenever a user messages the bot.
+The `Open` command can be used to open a report manually, or `Detain` if you want to mute them as well.
 
 ### Using a Report
 Once a report is opened, anyone with access to this private channel can talk with the user through the bot.
-Talking through WarmBot has the added benefit of masking staff identities.
-If things become hostile, the user will have no idea who they're talking to.
-
-![Staff](https://i.imgur.com/tRLMPwj.png)
+The user only needs to talk with the bot like a normal DM.
 
 #### Closing a Report
 ##### From Discord
  * Delete the channel - WarmBot will detect the event and close the report for you.
 
 ##### Using Commands
- * In the report channel, `close` - This has the same effect as deleting the channel.
- * In the report channel, `archive` - Transcribes the report to text, archives it, then closes the report.
+ * `Close` - This has the same effect as deleting the channel.
+ * `Archive` - Transcribes the report to text, archives it, then closes the report.
 
 ## Setup
-Refer to [warmbotsetup.md](warmbotsetup.md) for full setup instructions.
+Refer to the [setup](warmbotsetup.md) instructions.
 
 ## Commands
-Refer to [commands.md](commands.md) for a general list and explanation of all available commands.
-To learn about commands during runtime, use the `help` command!
+To see available commands, use `Help` or read the [commands](commands.md) documentation.
--- src/main/kotlin/me/aberrantfox/warmbot/messages/Messages.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/messages/Messages.kt
@@ -1,82 +1,77 @@
 package me.aberrantfox.warmbot.messages
 
 class Messages(
-    //General Descriptions
-    val BOT_DESCRIPTION: String = "",
-
     //Configuration commands descriptions
     val SET_REPORT_CATEGORY_DESCRIPTION: String = "",
     val SET_ARCHIVE_CHANNEL_DESCRIPTION: String = "",
     val SET_STAFF_ROLE_DESCRIPTION: String = "",
     val SET_LOGGING_CHANNEL_DESCRIPTION: String = "",
-    val ADD_STAFF_CHANNEL_DESCRIPTION: String = "",
-    val REMOVE_STAFF_CHANNEL_DESCRIPTION: String = "",
-    val LIST_STAFF_CHANNELS_DESCRIPTION: String = "",
 
     //Owner commands descriptions
     val SET_PRESENCE_DESCRIPTION: String = "",
+
+    //Report commands descriptions
     val CLOSE_DESCRIPTION: String = "",
     val ARCHIVE_DESCRIPTION: String = "",
     val NOTE_DESCRIPTION: String = "",
-
-    //Report commands descriptions
     val MOVE_DESCRIPTION: String = "",
     val TAG_DESCRIPTION: String = "",
     val RESET_TAGS_DESCRIPTION: String = "",
-    val OPEN_DESCRIPTION: String = "",
-    val DETAIN_DESCRIPTION: String = "",
-    val USER_DETAIN_MESSAGE: String = "",
 
     //Report helper commands descriptions
+    val OPEN_DESCRIPTION: String = "",
+    val DETAIN_DESCRIPTION: String = "",
     val RELEASE_DESCRIPTION: String = "",
     val CLOSE_ALL_DESCRIPTION: String = "",
+
+    //Info commands descriptions
     val INFO_DESCRIPTION: String = "",
     val IS_REPORT_DESCRIPTION: String = "",
     val PEEK_HISTORY_DESCRIPTION: String = "",
 
-    //Info commands descriptions
-    val SEND_MACRO_DESCRIPTION: String = "",
+    //Macro commands descriptions
     val ADD_MACRO_DESCRIPTION: String = "",
     val REMOVE_MACRO_DESCRIPTION: String = "",
-
-    //Macro commands descriptions
     val RENAME_MACRO_DESCRIPTION: String = "",
     val EDIT_MACRO_DESCRIPTION: String = "",
     val LIST_MACROS_DESCRIPTION: String = "",
-    val STATUS_DESCRIPTION: String = "Display network status and total uptime.",
-    val SET_REPORT_CATEGORY_SUCCESSFUL: String = "",
-    val SET_ARCHIVE_CHANNEL_SUCCESSFUL: String = "",
 
     //Utility commands descriptions
-    val SET_STAFF_ROLE_SUCCESSFUL: String = "",
+    val STATUS_DESCRIPTION: String = "",
 
     //Success message
+    val SET_REPORT_CATEGORY_SUCCESSFUL: String = "",
+    val SET_ARCHIVE_CHANNEL_SUCCESSFUL: String = "",
     val SET_LOGGING_CHANNEL_SUCCESSFUL: String = "",
+    val SET_STAFF_ROLE_SUCCESSFUL: String = "",
     val GUILD_SETUP_SUCCESSFUL: String = "",
+
+    //Fail message
     val FAIL_GUILD_SETUP: String = "",
     val FAIL_MISSING_CLEARANCE: String = "",
     val FAIL_COULD_NOT_FIND_ROLE: String = "",
-
-    //Fail message
     val FAIL_GUILD_NOT_CONFIGURED: String = "",
+
+    //Logging messages
     val STARTUP_LOG: String = "",
     val MEMBER_OPEN_LOG: String = "",
     val STAFF_OPEN_LOG: String = "",
     val ARCHIVE_LOG: String = "",
-
-    //Logging messages
     val COMMAND_CLOSE_LOG: String = "",
     val MANUAL_CLOSE_LOG: String = "",
     val COMMAND_LOG: String = "",
     val ERROR_LOG: String = "",
+
+    //Default setup info
     val DEFAULT_REPORT_CATEGORY_NAME: String = "",
     val DEFAULT_HOLDER_CATEGORY_NAME: String = "",
     val DEFAULT_ARCHIVE_CHANNEL_NAME: String = "",
     val DEFAULT_LOGGING_CHANNEL_NAME: String = "",
-
-    //Default setup info
-    val DEFAULT_COMMAND_CHANNEL_NAME: String = "",
     val DEFAULT_STAFF_ROLE_NAME: String = "",
+
+    //Misc
+    val DEFAULT_DISCORD_PRESENCE: String = "",
+    val BOT_DESCRIPTION: String = "",
     val DEFAULT_INITIAL_MESSAGE: String = "",
-    val DEFAULT_DISCORD_PRESENCE: String = ""
+    val USER_DETAIN_MESSAGE: String = ""
 )
