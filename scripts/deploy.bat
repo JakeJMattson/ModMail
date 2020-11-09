7c4eac77ac8c76e7552aebf1a30d56f1c6cc8f6a
JakeJMattson <JakeJMattson@gmail.com>
2020-11-08 23:29:29 -0600

    Auto-deploy to docker hub

--- scripts/deploy.bat
+++
@@ -1,2 +0,0 @@
-docker build -t modmail:latest -f docker/Dockerfile --no-cache .
-docker run -e BOT_TOKEN=%1 -v %2:/data modmail:latest
--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t modmail:latest -f docker/Dockerfile --no-cache .
-cmd="docker run -e BOT_TOKEN='$1' -v $2:/data modmail:latest"
-eval $cmd
