5c896da549aba9b737ef97a8d9b660bdd052a845
Jake Mattson <dev.ioparameters@simplelogin.fr>
2022-07-21 16:23:18 -0500

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ group = "me.jakejmattson"
 version = "4.0.0"
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
